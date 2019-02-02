2c56dbf5f53dfbac72687d5932980fceac60b7f1
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-02-02 20:26:38 +0000

    added cors

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ routes(app);
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const cors = require('cors')
 app.use(cors({
     origin: 'http://localhost',
     allowedHeaders: ["Content-Type", "Authorization"]
