9b14892ecc8cc1e317049c6a9c7edaa58dd8e3f5
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-02-02 20:04:25 +0000

    add cors header

--- server.js
+++ server.js
@@ -32,6 +32,12 @@ app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(port);
 
 console.log('Tarantino RESTful API server started on: ' + port);
